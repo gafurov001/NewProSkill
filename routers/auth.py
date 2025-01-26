93eddea2b6b337b0fb17be6fcbbeea150c3fc301
gafurov001 <fakhriddingafurv@gmail.com>
2025-01-26 05:28:14 -0800

    main

--- routers/auth.py
+++ routers/auth.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 auth_router = APIRouter()
 
 
-@auth_router.get("/hello_faxa/{name}")
+@auth_router.get("/hello_faxa2/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
